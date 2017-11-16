cf6c82e9cf92fd60d7e798937eb644b4ff8dec05
Jesús Leganés-Combarro <piranna@gmail.com>
2017-11-16 09:20:31 +0100

    Only calculate type and reset data on middleware instantiation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 export default function resetMiddleware (option) {
+  let resetType = 'RESET'
+  let resetData = 'state'
+
+  if ((typeof option === 'string' && option.length > 0) || typeof option === 'symbol') {
+    resetType = option
+  } else if (typeof option === 'object') {
+    resetType = typeof option.type === 'string' && option.type.length > 0 || typeof option === 'symbol' ? option.type : resetType
+    resetData = typeof option.data === 'string' && option.data.length > 0 ? option.data : resetData
+  }
+
   return (next) => (reducer, initialState, enhancer) => {
-    let resetType = 'RESET'
-    let resetData = 'state'
-
-    if ((typeof option === 'string' && option.length > 0) || typeof option === 'symbol') {
-      resetType = option
-    } else if (typeof option === 'object') {
-      resetType = typeof option.type === 'string' && option.type.length > 0 || typeof option === 'symbol' ? option.type : resetType
-      resetData = typeof option.data === 'string' && option.data.length > 0 ? option.data : resetData
-    }
-
     const enhanceReducer = (state, action) => {
       if (action.type === resetType) {
         state = action[resetData]
